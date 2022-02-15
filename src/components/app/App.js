e3a2c94be8551d46b5e44a1a702c48ba475409c1
Ajri Sylhasi <ajri@gmail.com>
2022-02-15 16:20:29 +0100

    Added layout for Login and Register

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,43 +1,42 @@
-import "./App.css";
 import {
   BrowserRouter as Router,
-  Routes,
+  Redirect,
   Route,
-  Navigate,
+  Switch,
 } from "react-router-dom";
 import React from "react";
-import { ThemeProvider } from "@mui/material/styles";
-import { Typography } from "@material-ui/core";
-import theme from "../../theme";
+// import { ThemeProvider } from "@mui/material/styles";
+// // import { Typography } from "@material-ui/core";
+// import theme from "../../theme";
 import Provider from "../provider/Provider";
+import Login from "../login/Login";
+import Register from "../register/Register";
 
 function App() {
   return (
     <Provider>
-      <ThemeProvider theme={theme}>
-        <div className="root" data-testid="app_test_id">
-          <Router>
-            <Routes>
-              <Route path={["/login"]}>
-                <Typography>Login</Typography>
-              </Route>
-              <Route path={["/reset-password/:email/:code", "/reset-password"]}>
-                <Typography>Reset Password</Typography>
-              </Route>
-              <Route path={["/register/:email/:code", "/register"]}>
-                <Typography>Register</Typography>
-              </Route>
-              <Route path={["", "/"]}>
-                <Typography>Main Page</Typography>
-                <p>hello</p>
-              </Route>
-              <Route>
-                <Navigate to="/" />
-              </Route>
-            </Routes>
-          </Router>
-        </div>
-      </ThemeProvider>
+      <div className="root" data-testid="app_test_id">
+        <Router>
+          <Switch>
+            <Route path={["/login"]}>
+              <Login />
+            </Route>
+            <Route path={["/reset-password/:email/:code", "/reset-password"]}>
+              <p>Reset Password</p>
+            </Route>
+            <Route path={["/register/:email/:code", "/register"]}>
+              <Register />
+            </Route>
+            <Route path={["", "/"]}>
+              <p>Main Page</p>
+              <p>hello</p>
+            </Route>
+            <Route>
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </Provider>
   );
 }
