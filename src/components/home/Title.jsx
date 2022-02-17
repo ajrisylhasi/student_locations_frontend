cddc1c2d9e21d2b6cf31a8c2472dbf4e3f67f58d
Ajri Sylhasi <ajri@gmail.com>
2022-02-17 02:46:55 +0100

    Moved files and fixed Title

--- src/components/home/Title.jsx
+++ src/components/home/Title.jsx
@@ -2,13 +2,11 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import Typography from "@mui/material/Typography";
 
-function Title({ children }) {
-  return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
-      {children}
-    </Typography>
-  );
-}
+const Title = ({ children }) => (
+  <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    {children}
+  </Typography>
+);
 
 Title.propTypes = {
   children: PropTypes.node.isRequired,
