7f1cbaadc143ecdbeca9178803ccf8090a75cd6c
Ajri Sylhasi <ajri@gmail.com>
2022-03-12 13:01:35 +0100

    Added Google Map React Api

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,39 +1,50 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import GoogleMapReact from "google-map-react";
 import Deposits from "./Deposits";
 
-const HomeContent = () => {
-  console.log("To add map");
+const { REACT_APP_MAPS_API_KEY } = process.env;
 
-  return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} md={4} lg={3}>
-        <Paper
-          sx={{
-            p: 2,
-            display: "flex",
-            flexDirection: "column",
-            height: 240,
-          }}
-        >
-          <Deposits />
-        </Paper>
-      </Grid>
-      <Grid item xs={12} md={4} lg={3}>
-        <Paper
-          sx={{
-            p: 2,
-            display: "flex",
-            flexDirection: "column",
-            height: 240,
-          }}
-          id="homeMap"
-        />
-      </Grid>
+const HomeContent = () => (
+  <Grid container spacing={3}>
+    <Grid item xs={12} md={4} lg={3}>
+      <Paper
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          height: 240,
+        }}
+      >
+        <Deposits />
+      </Paper>
     </Grid>
-  );
-};
+    <Grid item xs={12} md={4} lg={3}>
+      <Paper
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          height: 240,
+        }}
+      >
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: `${REACT_APP_MAPS_API_KEY}`,
+          }}
+          options={{ keyboardShortcuts: false }}
+          defaultCenter={{ lat: 47.5289, lng: 21.6254 }}
+          defaultZoom={14}
+        >
+          <p lat={47.5289} lng={21.6254}>
+            Ajri
+          </p>
+        </GoogleMapReact>
+      </Paper>
+    </Grid>
+  </Grid>
+);
 
 const Home = () => <HomeContent />;
 
